042f9e44e14e98eec2c43f06e1fc0615e47a454e
vasylenkodi <vasylenko1daniil@gmail.com>
2023-02-15 14:43:56 +0100

    добавляет решение задания 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick(event) {
+    intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+    
+    event.target.setAttribute("disabled", true);
+    refs.stopBtn.removeAttribute("disabled");
+}
+
+function onStopBtnClick(event) {
+    clearInterval(intervalId);
+    event.target.setAttribute("disabled", true);
+    refs.startBtn.removeAttribute("disabled");
+}
